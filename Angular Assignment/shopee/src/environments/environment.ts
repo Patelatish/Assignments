df40ae049a4bc39b07210f81ed75c3b135d24efa
Aatish Patel <87796069+Patelatish@users.noreply.github.com>
2022-04-01 17:47:29 +0530

    Add files via upload

---
+++ Angular Assignment/shopee/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCyZGfFsWXcyYoEeAAFUXQcXyf87_gxwJY",
+    authDomain: "shopee-a3e9b.firebaseapp.com",
+    projectId: "shopee-a3e9b",
+    storageBucket: "shopee-a3e9b.appspot.com",
+    messagingSenderId: "270600164537",
+    appId: "1:270600164537:web:a9e438a0f156b0799c7768",
+    measurementId: "G-19C4YC0JXL"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
